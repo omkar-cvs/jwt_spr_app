e4768d67a3424940ceb02ee6adca1eb536f81b3f
Omkar Singh <omkarsinghaws@gmail.com>
2024-09-01 16:03:43 +0530

    first commit

---
+++ web-app-all-be/src/main/java/com/oks/web/app/all/be/WebAppAllBeApplication.java
@@ -0,0 +1,22 @@
+package com.oks.web.app.all.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+@SpringBootApplication
+public class WebAppAllBeApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppAllBeApplication.class, args);
+	}
+	
+	   @Override
+	   protected SpringApplicationBuilder configure(SpringApplicationBuilder 
+	   application) {
+	      return application.sources(WebAppAllBeApplication.class);
+	   }
+
+}
